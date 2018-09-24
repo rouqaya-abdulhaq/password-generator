7423185a491a551d77727605d7ed79ea3802a120
Rouqaya Abdulhaq <ruqia334@gmail.com>
2018-09-24 14:59:40 +0300

    found the bugs in my code

--- code.js
+++ code.js
@@ -1,4 +1,38 @@
-let length = document.getElementById("length");
-let symbolCheck = document.getElementById("symbol");
-let submitButton = document.getElementById("submit");
-let output = document.getElementById("output");
+let passwordLength = document.querySelector("#length");
+let symbolCheck = document.querySelector("#symbol");
+let submitButton = document.querySelector("#submit");
+let output = document.querySelector("#output");
+const letters = "abcdefghijklmnopqrstuvwxyz";
+const symbols = "@#$&!";
+var characterTypes = ["letter","number"];
+
+const randomLetter = ()=>letters.charAt(Math.floor(Math.random()*letters.length));
+	
+const randomSymbol = ()=>symbols.charAt(Math.floor(Math.random()*symbols.length));
+
+const randomNumber = ()=>Math.floor(Math.random()*10);
+	 
+const isChecked =(checkBox)=>{
+	return checkBox.checked;
+}
+// //there is a bug in this function it's returning isChecked false despite being true
+const randomCharacterType =()=>{
+// 	if (isChecked(symbolCheck) === true) {characterTypes.push("symbol")}
+	return characterTypes[Math.floor(Math.random()*characterTypes.length)];
+}
+//there is an issue with passing the passwordlength parameter in this function
+const generatPassword =()=>{
+	var password ="";
+	while (password.length<8){
+		characterType = randomCharacterType();
+		if (characterType ==="symbol"){
+			password+= randomSymbol();
+		}else if(characterType==="number"){
+			password+= randomNumber();
+		}else{password+= randomLetter();}
+	}
+	return password;
+}
+//the password is being generated but it's not showing in the output
+submitButton.addEventListener("click",()=>{output.textContenet =generatPassword()}); 
+alert(generatPassword());
