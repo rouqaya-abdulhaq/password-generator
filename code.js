db6846e1460ee4befd5364d19c35c4b3da6a8862
Rouqaya Abdulhaq <ruqia334@gmail.com>
2018-09-25 11:34:14 +0300

    fixed the generate password function bug

--- code.js
+++ code.js
@@ -1,4 +1,3 @@
-let passwordLength = document.querySelector("#length");
 let symbolCheck = document.querySelector("#symbol");
 let submitButton = document.querySelector("#submit");
 let output = document.querySelector("#output");
@@ -20,10 +19,12 @@ const randomCharacterType =()=>{
 // 	if (isChecked(symbolCheck) === true) {characterTypes.push("symbol")}
 	return characterTypes[Math.floor(Math.random()*characterTypes.length)];
 }
-//there is an issue with passing the passwordlength parameter in this function
+
 const generatPassword =()=>{
-	var password ="";
-	while (password.length<8){
+	let userNumberInput = document.querySelector("#length");
+	let passwordLength = userNumberInput.value;
+	let password ="";
+	while (password.length<passwordLength){
 		characterType = randomCharacterType();
 		if (characterType ==="symbol"){
 			password+= randomSymbol();
@@ -33,6 +34,9 @@ const generatPassword =()=>{
 	}
 	return password;
 }
-//the password is being generated but it's not showing in the output
-submitButton.addEventListener("click",()=>{output.textContenet =generatPassword()}); 
-alert(generatPassword());
+const display = ()=>{
+	output.textContent = generatPassword();
+}
+//the password is being generated but it's disapearing in less than a second
+submitButton.addEventListener("click",display); 
+
