19f1d1ce28b64615e84c725be697cf865a660260
Rouqaya Abdulhaq <ruqia334@gmail.com>
2018-09-27 15:01:22 +0300

    fixed the isChecked bug and the display output bug

--- code.js
+++ code.js
@@ -1,9 +1,8 @@
-let symbolCheck = document.querySelector("#symbol");
 let submitButton = document.querySelector("#submit");
 let output = document.querySelector("#output");
 const letters = "abcdefghijklmnopqrstuvwxyz";
 const symbols = "@#$&!";
-var characterTypes = ["letter","number"];
+var characterTypes = ["letter","number","letter","number","letter"];
 
 const randomLetter = ()=>letters.charAt(Math.floor(Math.random()*letters.length));
 	
@@ -14,10 +13,13 @@ const randomNumber = ()=>Math.floor(Math.random()*10);
 const isChecked =(checkBox)=>{
 	return checkBox.checked;
 }
-// //there is a bug in this function it's returning isChecked false despite being true
+
 const randomCharacterType =()=>{
-// 	if (isChecked(symbolCheck) === true) {characterTypes.push("symbol")}
-	return characterTypes[Math.floor(Math.random()*characterTypes.length)];
+	let symbolCheck = document.querySelector("#symbol");
+	if (isChecked(symbolCheck) === true) {characterTypes.push("symbol")}
+	let character= characterTypes[Math.floor(Math.random()*characterTypes.length)];
+    if (isChecked(symbolCheck) === true) {characterTypes.pop()}
+    return character;
 }
 
 const generatPassword =()=>{
@@ -37,6 +39,7 @@ const generatPassword =()=>{
 const display = ()=>{
 	output.textContent = generatPassword();
 }
-//the password is being generated but it's disapearing in less than a second
+
 submitButton.addEventListener("click",display); 
+submitButton.addEventListener("click",function(event){event.preventDefault()});
 
