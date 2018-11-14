aafef904d03343dbda5c92462fd85f946811308b
Rouqaya Abdulhaq <ruqia334@gmail.com>
2018-11-14 13:48:40 +0300

    added the capital letter feature

--- code.js
+++ code.js
@@ -2,27 +2,39 @@ let submitButton = document.querySelector("#submit");
 let output = document.querySelector("#output");
 const letters = "abcdefghijklmnopqrstuvwxyz";
 const symbols = "@#$&!";
-var characterTypes = ["letter","number","letter","number","letter"];
+const capitalLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//to have more letters and numbers than anything in the password i repeated the words
+let characterTypes = ["letter","number","letter","number","letter"];
 
+//functions that return a random characters using the arrays above
 const randomLetter = ()=>letters.charAt(Math.floor(Math.random()*letters.length));
-	
 const randomSymbol = ()=>symbols.charAt(Math.floor(Math.random()*symbols.length));
-
+const randomCapital =()=> capitalLetters.charAt(Math.floor(Math.random()*capitalLetters.length));
 const randomNumber = ()=>Math.floor(Math.random()*10);
-//if you wanted to add more features to the app that the user can choose and check	 
+
+	 
 const isChecked =(checkBox)=>{
 	return checkBox.checked;
 }
+
 //uses the isChecked function to see if the user checked symbol and adds symbol to the characterTypes
 //and chooses a type randomly
-//you can add more features like that such as capital letters
+//you can add more features like that 
 const randomCharacterType =()=>{
 	let symbolCheck = document.querySelector("#symbol");
+	let capitalCheck = document.querySelector("#capital-letter");
+	//if the user checked the symbol checkbox
 	if (isChecked(symbolCheck) === true) {characterTypes.push("symbol")}
+    //if the user checked the capital letter checkbox	
+    if (isChecked(capitalCheck) === true) {characterTypes.push("capitalLetter")}
+
 	let character= characterTypes[Math.floor(Math.random()*characterTypes.length)];
-    if (isChecked(symbolCheck) === true) {characterTypes.pop()}
+    //to delete the characters after they have been checked so they dont fill the array 
+    if (isChecked(capitalCheck) === true) {characterTypes.pop()}
+    if (isChecked(symbolCheck) === true) {characterTypes.pop()}	
     return character;
 }
+
 //generates a password 6 to 16 characters that starts with a letter
 const generatPassword =()=>{
 	let userNumberInput = document.querySelector("#length");
@@ -32,9 +44,12 @@ const generatPassword =()=>{
 		characterType = randomCharacterType();
 		if (characterType ==="symbol"){
 			password+= randomSymbol();
-		}else if(characterType==="number"){
-			password+= randomNumber();
-		}else{password+= randomLetter();}
+		}else if (characterType==="capitalLetter"){
+			password+= randomCapital();
+			}
+			else if(characterType==="number"){
+				password+= randomNumber();
+			}else{password+= randomLetter();}
 	}
 	return password;
 }
